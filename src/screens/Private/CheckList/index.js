1a5534fdbc91d5618e6c0ead482b1695331f1652
LeonardoASC <leonardoaschaves@gmail.com>
2024-03-27 18:34:05 -0300

    feat: quase terminando tela inicial de checklist

--- src/screens/Private/CheckList/index.js
+++ src/screens/Private/CheckList/index.js
@@ -1,27 +1,107 @@
-import react from "react";
-import { Image, SafeAreaView, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { Header, Container, ImageHeader } from "./styles.js";
-import startChecklist from '../../../../assets/startChecklist.png';
+import React from 'react';
+import { Image, SafeAreaView, Text, TouchableOpacity, StyleSheet, View, FlatList } from "react-native";
+import { Header, Container, ImageHeader, StyledItemContainer, StyledItemText } from "./styles.js";
+import startChecklist from '../../../../assets/startChecklist2.png';
+import capoAberto from '../../../../assets/capoAberto.png';
+import capoabertoestrada from '../../../../assets/capoabertoestrada.png';
+import mecanicomaos from '../../../../assets/mecanicomaos.png';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export function CheckList({ navigation }) {
+    const dicas = [
+        {
+            id: '1',
+            title: 'Dica 1',
+            description: 'Para utilizar o aplicativo',
+            imagem: capoAberto
+        },
+        {
+            id: '2',
+            title: 'Dica 2',
+            description: 'Para utilizar o aplicativo',
+            imagem: capoabertoestrada
+        },
+        {
+            id: '3',
+            title: 'Dica 3',
+            description: 'Para utilizar o aplicativo',
+            imagem: mecanicomaos
+        },
+    ];
+
+    const renderItem = ({ item }) => (
+        <StyledItemContainer>
+            <StyledItemText>{item.title}</StyledItemText>
+            <StyledItemText>{item.description}</StyledItemText>
+            <Image
+                source={item.imagem}
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    position: 'absolute',
+                    zIndex: -1,
+                    // aspectRatio: 1, 
+                    // resizeMode: 'cover' 
+                }}
+            />
+        </StyledItemContainer>
+
+    );
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Header>
                 <ImageHeader source={startChecklist} style={StyleSheet.absoluteFill} />
-                <TouchableOpacity style={{
-                    width: '90%',
-                    height: '20%',
-                    backgroundColor: 'green',
-                    borderRadius: 5,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-                    onPress={() => navigation.navigate('CheckListOne')}>
-                    <Text style={{ color: 'white' }}>Iniciar Check List</Text>
-                </TouchableOpacity>
+                <View style={{ width: '90%', height: '25%', alignItems: 'center', justifyContent: 'center' }}>
+                    <MaterialCommunityIcons name="hand-wave" size={24} color="#DEAC38" />
+                    <Text style={{
+                        color: 'white',
+                        fontSize: 24,
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                    }}>Olá Usuário,</Text>
+                    <Text style={{
+                        color: 'white',
+                        textAlign: 'center',
+                        textShadowColor: 'rgba(0, 0, 0, 1)', textShadowOffset: { width: 8, height: 4 }, textShadowRadius: 15
+                    }}>aqui você poderá realizar um checklist do carro escolhido!</Text>
+
+                    <TouchableOpacity style={{
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: '#39BF61',
+                        borderRadius: 20,
+                        alignItems: 'center',
+                        flexDirection: 'row',
+                        top: '40%'
+                    }} onPress={() => navigation.navigate('CheckListOne')}>
+                        <View style={{ width: '20%', alignItems: 'center', justifyContent: 'center' }}>
+                            <Ionicons name="car" size={24} color="white" />
+                        </View>
+                        <View style={{ width: '60%', justifyContent: 'center' }}>
+                            <Text style={{ color: 'white', fontSize: 24, fontWeight: 'bold' }}>Iniciar Check List</Text>
+                            <Text style={{ color: 'white' }}>Checklist para auxiliar na analise dos veiculos</Text>
+                        </View>
+                        <View style={{ width: '20%', alignItems: 'center', justifyContent: 'center' }}>
+                            <Ionicons name="arrow-forward" size={24} color="white" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </Header>
             <Container>
-                <Text>1</Text>
+                <View style={{ width: '100%', height: '90%', marginTop: '10%', alignItems: 'center' }}>
+                    <View style={{ width: '90%' }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 24 }}>Dicas</Text>
+                        <Text style={{ marginTop: 5, color: 'gray' }}>Pontos importantes</Text>
+                    </View>
+                    <FlatList
+                        data={dicas}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id.toString()}
+                        horizontal
+                    />
+                </View>
             </Container>
         </SafeAreaView>
     )
--- src/screens/Private/CheckList/styles.js
+++ src/screens/Private/CheckList/styles.js
@@ -7,16 +7,14 @@ export const Header = styled.View`
     width: 100%;
     height: 50%;
     flex-direction: row;
-    align-items: flex-end;
+    align-items: center;
     justify-content:center;
 `;
 
 export const Container = styled.View`
-  height: 80%;
+  height: 45%;
   width: 100%;
-  align-items: center;
-  justify-content: center;
-
+  margin-top: 10%;
 `;
 
 
@@ -24,4 +22,28 @@ export const ImageHeader = styled.Image`
   width: 100%;
   height: 100%;
 
+`;
+
+export const StyledItemContainer = styled.View`
+    background-color: #ffffff;
+    padding: 20px;
+    border-radius: 10px;
+    height: 80%;
+    margin: 10px;
+    ${Platform.select({
+  ios: `
+      shadow-color: #000;
+      shadow-offset: 0px 2px;
+      shadow-opacity: 0.1;
+      shadow-radius: 2px;
+    `,
+  android: `
+      elevation: 4;
+    `,
+})}
+`;
+
+export const StyledItemText = styled.Text`
+    font-size: 16px;
+    color: white;
 `;
